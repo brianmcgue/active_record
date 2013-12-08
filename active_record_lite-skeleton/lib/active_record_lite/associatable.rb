719298725aaedadd239628fd19eefcd574839d90
App Academy Student <student@appacademy.io>
2013-12-08 13:59:10 -0800

    Associatable#belongs_to now works.

--- active_record_lite-skeleton/lib/active_record_lite/associatable.rb
+++ active_record_lite-skeleton/lib/active_record_lite/associatable.rb
@@ -1,19 +1,23 @@
 require 'active_support/core_ext/object/try'
 require 'active_support/inflector'
 require_relative './db_connection.rb'
+require 'debugger'
 
 class AssocParams
   def other_class
+    self.params[:class_name].constantize
   end
 
   def other_table
+    self.other_class.table_name
   end
 end
 
 class BelongsToAssocParams < AssocParams
+  attr_accessor :name, :params
   def initialize(name, params)
     new_params = {
-      :class_name => name,
+      :class_name => name.to_s.split("_").map{|w| w.capitalize}.join(""),
       :foreign_key => "#{name}_id",
       :primary_key => "id"
     }
@@ -43,6 +47,22 @@ module Associatable
   end
 
   def belongs_to(name, params = {})
+    aps = BelongsToAssocParams.new(name, params)
+    define_method(name) do
+      key = self.send(aps.params[:foreign_key])
+      puts key
+      # debugger
+      results = DBConnection.execute(<<-SQL, key)
+        SELECT
+          *
+        FROM
+          #{aps.other_table}
+        WHERE
+          #{aps.other_table}.#{aps.params[:primary_key]} = ?
+      SQL
+
+      aps.other_class.parse_all(results).first
+    end
   end
 
   def has_many(name, params = {})
